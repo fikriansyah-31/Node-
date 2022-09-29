52161e38cff3c0679a5445b1d2dfbe4376b12852
fikriansyah-31 <syafanurwalinda23@gmail.com>
2022-09-29 11:18:56 +0700

    Promise

--- One.js
+++ One.js
@@ -1,13 +1,21 @@
-const fetchData = callback => {
-    setTimeout(() => {
-        callback('Done!')
-    }, 1500);
+const fetchData = () => {
+    const promise = new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done!')
+        }, 1500);
+    })
+    return promise
 };
 
 setTimeout(() => {
     console.log('Timer is Done!');
-    fetchData(text => {
+    fetchData()
+        .then(text => {
         console.log(text);
+    return fetchData();
+    })
+        .then(text2 => {
+        console.log(text2);
     });
 }, 2000);
 
