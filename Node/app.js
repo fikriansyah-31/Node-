dcb79647fba232c3b15f4de54f578db7fb4b99b3
fikriansyah-31 <syafanurwalinda23@gmail.com>
2022-09-30 16:51:03 +0700

    Parsing Request Bodies

--- Node/app.js
+++ Node/app.js
@@ -13,7 +13,16 @@ const server = http.createServer((req, res) => {
         return res.end()
     }
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'FIKRI GANTENG');
+        const body = [] //Array
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk); //Chunk = Bongkahan bongkahan
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString(); //Di rubah ke string
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        })
         res.statusCode = 302;
         res.setHeader('Location', '/')
         return res.end()
